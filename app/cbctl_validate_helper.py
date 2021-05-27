dbc0308ce0ebfd27f9daabcb2e6d11288373cc71
root <root@j-serv.pandora.local>
2021-05-27 10:57:31 -0400

    added cbct_validate_helper.py

---
+++ app/cbctl_validate_helper.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+with open(sys.argv[1]) as f:
+    try:
+        data = json.load(f)
+    except Exception as e:
+        print("No violations reported!")
+        exit()
+
+    if len(data['policy_violations']) > 0:
+        print("*Cbctl validate results*\n")
+
+        for rule in range(len(data['policy_violations'])):
+            for policy in range(len(data['policy_violations'])):
+                print("CBC Policy: " + str(data['policy_violations'][rule]['policy']) +
+                      "\nRules violated: " + str(data['policy_violations'][rule]['rule']))
+
+
+    else:
+        print("Review ${env.BUILD_JOB} for full details on cbctl results")
